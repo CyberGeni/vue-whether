3454f1a00878f013539e89f2b6a0d348e2755cbf
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-01-10 21:36:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# whether
+
+> A weather application built with vue.js displaying the temperature, date and background video animation. It recieves input from the user through a search box, and calls the API for the city requested.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>Whether</h1>
+    <h2>Coming soon...</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+  
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Rubik', 'Segoe UI';
+  }
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 100%;
+    height: 100vh;
+    background-image: linear-gradient(135deg, rgba(14, 51, 216, 0.623), dodgerblue, rgba(52, 2, 218, 0.603));
+    color: white;
+    font-weight: 300;
+  }
+</style>
